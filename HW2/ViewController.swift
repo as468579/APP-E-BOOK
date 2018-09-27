0387946c4084a8b505af7c5f1ca4dd8c83064419
User05 <user05@user05de-imac.local>
2018-09-27 18:15:11 +0800

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by User05 on 2018/9/27.
+//  Copyright © 2018年 User05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
